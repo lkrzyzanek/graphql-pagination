20e80771485c4d19bea467acecb023eeca6c0b7b
Libor Krzyzanek <libor.krzyzanek@gmail.com>
2023-08-21 11:42:57 +0200

    fix: pager args first/last validation on non-negative number #123

---
+++ packages/core/__tests__/test-pager-args.ts
@@ -0,0 +1,33 @@
+import { GraphQLError } from "graphql";
+import {ArrayDataSource, dataSourcePager} from "../src";
+
+describe("pager-args", () => {
+
+
+    test("first-non-negative", async () => {
+        const pager = dataSourcePager<{ id: number }, number>({dataSource: new ArrayDataSource<{ id: number }, number>([{"id": 1}]), typeName: "TEST"});
+
+        try {
+            await pager.forwardResolver({first: -1});
+            throw new Error("no error thrown");
+        } catch (e) {
+            expect(e.message).toBe("first argument has to be a non-negative number");
+            const ex = e as GraphQLError;
+            expect(ex.extensions).toStrictEqual({ code: "BAD_USER_INPUT" });
+        }
+    });
+
+    test("last-non-negative", async () => {
+        const pager = dataSourcePager<{ id: number }, number>({dataSource: new ArrayDataSource<{ id: number }, number>([{"id": 1}]), typeName: "TEST"});
+
+        try {
+            await pager.backwardResolver({last: -1});
+            throw new Error("no error thrown");
+        } catch (e) {
+            expect(e.message).toBe("last argument has to be a non-negative number");
+            const ex = e as GraphQLError;
+            expect(ex.extensions).toStrictEqual({ code: "BAD_USER_INPUT" });
+        }
+    });
+
+});

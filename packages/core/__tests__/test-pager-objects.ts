bdc397c5da5fd48c8eed3492d4794041a3f6da79
Andy Stanberry <andystanberry@gmail.com>
2024-11-14 20:52:29 +0000

    feat: Pass args to getId

--- packages/core/__tests__/test-pager-objects.ts
+++ packages/core/__tests__/test-pager-objects.ts
@@ -1,4 +1,22 @@
-import { ArrayDataSource, DefaultCursorEncoderDecoder, PagerObject } from "../src";
+import { ArgsBackward, ArgsForward, ArrayDataSource, DataSourceBase, DefaultCursorEncoderDecoder, PagerObject } from "../src";
+
+class MockDataSource extends DataSourceBase<{ id: string }, { id: string }, ArgsForward, ArgsBackward > {
+    constructor() {
+        super()
+    }
+    async after() {
+        return [];
+    }
+    async before() {
+        return [];
+    }
+    async totalCount() {
+        return 0;
+    }
+
+    getId = jest.fn();
+}
+
 
 describe("pager-objects", () => {
 
@@ -11,5 +29,13 @@ describe("pager-objects", () => {
         expect(edge.cursor).not.toBeUndefined();
         expect(edge.node).not.toBeUndefined();
     });
+    
+    test("getId is called with args", () => {
+        const dataSource = new MockDataSource();
+        const data = [{ "id": "1" }];
+        const args = { some: 'args'}
+        PagerObject.connectionObject(data, args, 111, false,false, dataSource, new DefaultCursorEncoderDecoder());
+        expect(dataSource.getId).toHaveBeenCalledWith(data[0], args);
+    })
 
 });
